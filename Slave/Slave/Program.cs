1295c86bd27b69a8153068b3591b1aeb87005203
sofu0185 <sofu0185@edu.easj.dk>
2019-09-18 09:36:14 +0200

    setup

---
+++ Slave/Slave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Slave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
